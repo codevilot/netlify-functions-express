3502c245fb8944aa72ef7231a54bf705932ae29f
codevil <namhundred@naver.com>
2022-10-20 21:46:25 +0900

    init

--- public/utils/createElement.js
+++
@@ -1,7 +0,0 @@
-const createElement = domString => {
-  const $temp = document.createElement('template');
-  $temp.innerHTML = domString;
-  return $temp.content;
-};
-
-export default createElement;
