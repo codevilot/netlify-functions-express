3bf04f12bf8fbc3837047567a0a13181baae0845
codevil <namhundred@naver.com>
2022-10-20 19:57:39 +0900

    reload

--- express/server.js
+++ express/server.js
@@ -7,9 +7,8 @@ const bodyParser = require("body-parser");
 
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
-  res.end();
+  const { page } = req.params;
+  res.sendFile(path.join(__dirname, `public/database/${page}.json`));
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
